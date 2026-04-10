a4ce381e96ef97a57b6a928bac722326d2f42602
Vicky Han <vickyhan@VickydeMacBook-Pro.local>
2026-04-10 01:24:32 -0400

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/BikiniBottom_Library/',
   server: {
     host: "::",
     port: 8080,
